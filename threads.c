951cf1732256b28c3a9708cbf1cdb0b0fedc0a43
Mary Nansikombi <107887740+nansikom@users.noreply.github.com>
2024-11-14 19:41:56 -0800

    Update threads.c

--- threads.c
+++ threads.c
@@ -10,26 +10,7 @@ pthread_cond_t myCond1 = PTHREAD_COND_INITIALIZER;
 pthread_cond_t myCond2 = PTHREAD_COND_INITIALIZER;
 int iter= 0;
 int maxiter= 10; 
-//int buffer;
-//points to the variable in which the variable of the new thread is written into
-// int pthread_create( pthread_t* thread,
-// // points to the thread attribute struct that has the flags
-// const pthread_attr_t* attr,
-// // points to the function that will be the start point of the thread execution for the new thread.
-// void* (*start_routine) (void *),
-// //points to the arguments that will be passed to start routine and if you have many arguments jst pass in a struct instead.
-// void* arg 
-// );
 
-// while (1)
-// {
-// pthread_mutex_lock(&mutex); // Lock the mutex before checking if the buffer has data
-// while (count == 0) // Buffer is empty. Wait for signal that the buffer has data
-// pthread_cond_wait(&has_data, &mutex);
-// value = get_item(); // There's an item, get it! This will decrement int count
-// pthread_mutex_unlock(&mutex); // Unlock the mutex
-// pthread_cond_signal(&space_available); // Signal that the buffer has space
-// }
 void *consumerThread(void *arg);
 /*
 mymutex
